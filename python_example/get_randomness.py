ab5d418f8b9b751cd843d5e93f7e9288ceab0909
Jasper Palfree <well.caffeinated@gmail.com>
2025-02-26 13:51:45 -0700

    update python randomness example to use 3 sources

--- python_example/get_randomness.py
+++ python_example/get_randomness.py
@@ -1,13 +1,20 @@
-# just outputs 512 bits of system randomness
-
 import os
 import sys
+import hashlib
 
 def main():
-    # get randomness
-    randomness = os.urandom(64)
-    # output randomness
-    sys.stdout.buffer.write(randomness)
+    # get randomness from 3 sources
+    randomness1 = os.urandom(64)
+    randomness2 = os.urandom(64)  # additional source
+    randomness3 = os.urandom(64)  # additional source
+
+    # combine the randomness
+    combined_randomness = randomness1 + randomness2 + randomness3
+
+    # hash the combined randomness to ensure it's consistent in size
+    hashed_randomness = hashlib.sha3_512(combined_randomness).digest()
+    # output hashed randomness
+    sys.stdout.buffer.write(hashed_randomness)
     sys.stdout.buffer.flush()
 
 if __name__ == "__main__":
